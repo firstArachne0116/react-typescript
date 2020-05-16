138653f61eb5badadadb736ef0aea31408632ad6
firstarachne0116 <firstarachne0116@gmail.com>
2020-05-16 22:33:13 +0800

    Use local timezone instead of UTC-5

--- src/lib/words.ts
+++ src/lib/words.ts
@@ -14,14 +14,14 @@ export const isWinningWord = (word: string) => {
 
 export const getWordOfDay = () => {
   // January 1, 2022 Game Epoch
-  const epochMs = 1641013200000
+  const epochMs = new Date('January 1, 2022 00:00:00').valueOf()
   const now = Date.now()
   const msInDay = 86400000
   const index = Math.floor((now - epochMs) / msInDay)
   const nextday = (index + 1) * msInDay + epochMs
 
   return {
-    solution: WORDS[index % WORDS.length].toUpperCase(),
+    solution: WORDS[index].toUpperCase(),
     solutionIndex: index,
     tomorrow: nextday,
   }
